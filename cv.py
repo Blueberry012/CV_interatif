058d410dcc5a1a5fd3204064f48a3e3a4fdce82e
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2024-01-24 16:42:54 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -54,25 +54,12 @@ st.subheader("Alternant Data Analyst")
 st.write("Après 2 ans en tant qu'alternant Data Analyst, je recherche une nouvelle alternance en tant que Data Scientist à partir de septembre 2024 sur une période de 3 ans.")
 
 st.subheader("Diplômes, Formations et Projets")
-items = [
-    {"id": 1, "content": "BAC Général", "Diplôme":"BAC Spécialités Mathématiques et Sciences Économiques et Sociales", "Mention": "Admis Mention Bien", "Etablissement": "Lycée privé Montalembert", "Lieu": "Courbevoie (92400)","start": "2021-07-01"},
-    {"id": 2, "content": "BUT SD (ex. STID)", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Parcours": "Visualisation et Conception d'Outils Décisionnel","Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2021-09-01"},
-    {"id": 3, "content": "Mise en œuvre d’une enquête", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2021-11-01"},
-    {"id": 4, "content": "Création d'un reporting", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2022-01-01"},
-    {"id": 5, "content": "Analyse de données et datavisualisation", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2022-04-01"},
-    {"id": 6, "content": "Alternance", "Poste": "Alternant Data Analyst" ,"Entreprise":"Gan Assurances", "Lieu": "Nanterre (92000)", "start": "2022-09-01"},
-    {"id": 7, "content": "Mise en œuvre d’un processus de Datamining", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2023-11-01"},
-    {"id": 8, "content": "Conception d'un outil décisionnel", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2024-01-01"},
-    {"id": 9, "content": "Migration de données vers ou depuis un environnement NoSQL", "Formation":"BUT Sciences des Données, Statistique et Informatique Décisionnelle", "Etablissement": "IUT de Paris – Rives de Seine", "Lieu": "Paris 16e", "start": "2024-02-01"}
-]
 
-#timeline = st_timeline(items, groups=[], options={}, height="300px")
-#st.write(timeline)
 with open('example.json', "r") as f:
     data = f.read()
 
 # render timeline
-timeline(data, height=800)
+timeline(data, height=300)
 
 st.subheader("Expérience Professionnel")
 st.write("- Création d'un robot automatisant les tâches récurrentes avec Rstudio")
@@ -93,7 +80,7 @@ with st.expander("Machine Learning"):
     st.write("- Mise en œuvre d'un processus de Datamining (Construction de la base d'étude, Discrétisation, Modélisation, Exploitation du score)")
 
 
-st.subheader("Projet Universitaire")
+st.subheader("Projets Universitaires")
 
 with st.expander("Conception d'un outil décisionnel"):
     st.write("- Transformation de la donnée pour la mettre en conformité avec des normes (anonymisation, normalisation)")
@@ -113,21 +100,6 @@ with st.expander("Migration de données vers ou depuis un environnement NoSQL"):
     st.write("- Développement des programmes de migration des données")
     st.write("- Réalisation des scripts de requêtage de la nouvelle base de données")
 
-with st.expander("Mise en œuvre d’une enquête"):
-    st.write("- Réalisation d’un questionnaire sur LimeSurvey à propos du devenir d’anciens")
-    st.write("- Conception et nettoyage de la base de données.")
-    st.write("- Modélisation d’études sur Excel pour réaliser une enquête statistique.")
-    st.write("- Présentation orale des résultats.")
-
-with st.expander("Création d'un reporting"):
-    st.write("- Récupération des données d’un loueur pour développer une entreprise fictive.")
-    st.write("- Exploitation de la base de données en créant des requêtes sur SQL.")
-    st.write("- Conduite d’une étude statistique à l’aide d’Excel pour réaliser des synthèses numériques et graphiques.")
-
-with st.expander("Analyse de données et datavisualisation"):
-    st.write("- Web scrapping d’un site avec python pour étudier la performance de coureurs du 800m.")
-    st.write("- Exportation et nettoyage de la base de données sur R pour créer une base de données propre à partir des données brutes sous format CSV.")
-    st.write("- Création des codes pour l’analyse statistique des données nécessitant la gestion des librairies, packages, listes et dictionnaires.")
 st.divider()
 st.write("Pour plus d'information concernant mes projets et mon parcours, je vous invite à conculter mon Portfolio")
 st.link_button("Accéder à mon Porfolio", "https://fancy-trifle-4c29ba.netlify.app/")
