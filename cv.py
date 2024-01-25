777211883499831350079fddab5d26612cddff19
Blueberry012 <156487524+Blueberry012@users.noreply.github.com>
2024-01-25 14:27:52 +0100

    Update cv.py

--- cv.py
+++ cv.py
@@ -13,23 +13,8 @@ with st.sidebar:
     st.write("📅 20 ans")
     st.link_button("Consulter mon Porfolio", "https://thom-chhun-portfolio.netlify.app")
     st.link_button("Consulter mon Linkedin", "https://www.linkedin.com/in/thom-chhun-b7a587233")
-    st.download_button(
-        label="Télécharger mon CV",
-        key="download_button",
-        on_click=None,
-        file_name="CV_Thom_Chhun.pdf",
-        data="Images/CV_Thom_Chhun.pdf",
-        help="Mon CV",
-    )
-        
-        
-    with open("CV_Thom_Chhun.pdf", "rb") as file:
-        btn=st.download_button(
-        label="Télécharger mon CV",
-        data=file,
-        file_name="CV_Thom_Chhun.pdf",
-        mime="Images/CV_Thom_Chhun.pdf"
-    )
+    with open("Images/CV_Thom_Chhun.pdf", "rb") as f:
+        st.download_button("Télécharger mon CV", f, "CV_Thom_Chhun.pdf")
 
     with st.expander("Outils"):
         st.subheader("Data Mining et Analyse de données :")
